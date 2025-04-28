99ac6473bed902d423769cbc54e83c2ad9591bb8
James Randall <james.randall@blackdotsolutions.com>
2025-04-28 13:55:04 +0100

    Bumped to .NET 8

--- csharp-api/AddressBookChallenge/Models/UkAddress.cs
+++ csharp-api/AddressBookChallenge/Models/UkAddress.cs
@@ -4,22 +4,22 @@ public class UkAddress
 {
     // Required, maximum length 50 characters
     // Example: Mrs. Elizabeth White
-    public string Addressee { get; set; }
+    public string Addressee { get; set; } = string.Empty;
     
     // Required, maximum length 50 characters
     // Example: Hathaway Cottage
-    public string Street1 { get; set; }
+    public string Street1 { get; set; } = string.Empty;
     
     // Optional, maximum length 50 characters
     // Example: 1 Main Street
-    public string? Street2 { get; set; }
+    public string? Street2 { get; set; } = null;
     
     // Required, maximum length 50 characters
-    public string Town { get; set; }
+    public string Town { get; set; } = string.Empty;
     
     // Optional, maximum length 50 characters
-    public string County { get; set; }
+    public string County { get; set; } = string.Empty;
     
     // Required, maximum length 8 characters, only letters and numbers are allowed
-    public string Postcode { get; set; }
+    public string Postcode { get; set; } = string.Empty;
 }
